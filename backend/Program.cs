8c91538971dfa827d46b879b131107d3ccc2402a
Clarissa <clarissa_coda@yahoo.com.br>
2026-09-02 15:41:44 -0600

    feat: inject counter as a singleton to application

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,16 @@
+using rrm_reborn.backend;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IClickTracker, ClickTracker>();
+
 var app = builder.Build();
+app.UseHttpsRedirection();                          // enforcing https use
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
